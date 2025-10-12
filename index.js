af1e8b4451c753c5687fb49347f40edd518ed83f
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-10-13 00:06:11 +0530

    four commit one

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ init(); init(); // logs "initialized" only once
 console.log("munasinghe rajapaksha sachintha lakshan")
 
 
-console.log("seven");
-console.log("eight");
-console.log("nine");
-console.log("ten");
+console.log("sen");
+console.log("ght");
+console.log("ne");
+console.log("tn");
 
 
 
