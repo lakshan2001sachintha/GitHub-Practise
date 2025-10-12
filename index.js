7f2ccfbb13708fb1995547485b68a4ec02438944
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-10-12 20:18:48 +0530

    add my name

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ const onResize = debounce(() => console.log('resized'), 200);
 const init = once(() => console.log('initialized'));
 init(); init(); // logs "initialized" only once
 
+
+console.log("lakshan sachintha");
 await sleep(500); // pauses ~500ms inside async function
