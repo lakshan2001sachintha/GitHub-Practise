757a6394d545760b610f0bdd8f8b8cd0e12177d1
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-10-12 20:54:59 +0530

    commit again

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ import { greet, sum, debounce, once, sleep } from './small.js';
 console.log(greet('Asha'));        // "Hello, Asha!"
 console.log(sum(1,2,3,4));         // 10
 
-const onResize = debounce(() => console.log('resized'), 200);
-// window.addEventListener('resize', onResize);
-
-const init = once(() => console.log('initialized'));
-init(); init(); // logs "initialized" only once
 
 console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
