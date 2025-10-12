e8d9cb748c75eabaf2623dd316bf4228ca4eadd8
Sachintha_Lakshan <sachintha2001lakshan@gmail.com>
2025-10-12 21:03:54 +0530

    commit 9.03

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ console.log(greet('Asha'));        // "Hello, Asha!"
 console.log(sum(1,2,3,4));         // 10
 
 
+
+const init = once(() => console.log('initialized'));
+init(); init(); // logs "initialized" only once
+
 console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
 
 await sleep(500); // pauses ~500ms inside async function
